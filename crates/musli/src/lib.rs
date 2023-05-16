cd6fe6763829fe0ea2e9697a56e27e504612ec1f
John-John Tedro <udoprog@tedro.se>
2023-05-16 21:30:14 +0200

    Release 0.0.44

--- crates/musli/src/lib.rs
+++ crates/musli/src/lib.rs
@@ -38,8 +38,8 @@
 //! Add the following to your `Cargo.toml`:
 //!
 //! ```toml
-//! musli = "0.0.43"
-//! musli-wire = "0.0.43"
+//! musli = "0.0.44"
+//! musli-wire = "0.0.44"
 //! ```
 //!
 //! <br>
