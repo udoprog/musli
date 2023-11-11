3b504517d4722a94e86fe3914cca2124148e6792
John-John Tedro <udoprog@tedro.se>
2023-11-11 19:00:48 +0100

    zerocopy: Add documentation for Slice

--- crates/musli-zerocopy/src/slice/slice.rs
+++ crates/musli-zerocopy/src/slice/slice.rs
@@ -4,60 +4,214 @@ use crate::pointer::{Ref, Size};
 use crate::traits::ZeroCopy;
 
 mod sealed {
+    use crate::endian::ByteOrder;
+    use crate::pointer::{Ref, Size};
+    use crate::slice::Packed;
+    use crate::traits::ZeroCopy;
+
     pub trait Sealed {}
-    impl<T> Sealed for [T] {}
 
-    /// Helper trait to differentiate between the unsized slice `[T]` type and the
-    /// item it contains.
-    pub trait UnsizedSlice: Sealed {
-        /// The item in an unsized slice, or the `T` in `[T]`.
-        type Item;
-    }
+    impl<T, E: ByteOrder, O: Size> Sealed for Ref<[T], E, O> where T: ZeroCopy {}
 
-    impl<T> UnsizedSlice for [T] {
-        type Item = T;
-    }
+    impl<T, O: Size, L: Size, E: ByteOrder> Sealed for Packed<[T], O, L, E> {}
 }
 
 /// A trait implemented by slice-like types.
-pub trait Slice<T: ?Sized + self::sealed::UnsizedSlice>:
-    Copy + ZeroCopy + Load<Target = T>
-{
-    /// An item inside of the slice.
-    type ItemRef: Load<Target = T::Item>;
+pub trait Slice: self::sealed::Sealed + Copy + ZeroCopy + Load<Target = [Self::Item]> {
+    /// The item in an unsized slice, or the `T` in `[T]`.
+    type Item;
 
-    /// Construct a slice from a [`Ref<[T]>`].
-    fn from_ref<E: ByteOrder, O: Size>(slice: Ref<[T::Item], E, O>) -> Self
+    /// A returned reference to an item in a slice.
+    type ItemRef: Load<Target = Self::Item>;
+
+    /// Construct a slice from a [`Ref<[Self::Item]>`].
+    ///
+    /// # Panics
+    ///
+    /// This method panics if construction of the slice would overflow any of
+    /// its parameters.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use musli_zerocopy::{Ref, ZeroCopy};
+    /// use musli_zerocopy::slice::Slice;
+    ///
+    /// fn generic<S>(r: Ref<[S::Item]>) -> S where S: Slice, S::Item: ZeroCopy {
+    ///     S::from_ref(r)
+    /// }
+    /// ```
+    fn from_ref<E: ByteOrder, O: Size>(slice: Ref<[Self::Item], E, O>) -> Self
     where
-        T::Item: ZeroCopy;
+        Self::Item: ZeroCopy;
 
-    /// Construct a slice from its metadata.
+    /// Construct a slice from its `offset` and `len`.
+    ///
+    /// # Panics
+    ///
+    /// This method panics if construction of the slice would overflow any of
+    /// its parameters.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use musli_zerocopy::Ref;
+    /// use musli_zerocopy::slice::Slice;
+    ///
+    /// fn generic<S>() -> S where S: Slice {
+    ///     S::with_metadata(0, 10)
+    /// }
+    /// ```
     fn with_metadata(offset: usize, len: usize) -> Self;
 
-    /// Access an item in the slice.
+    /// Try to get a reference directly out of the slice without validation.
+    ///
+    /// This avoids having to validate every element in a slice in order to
+    /// address them.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use musli_zerocopy::{Buf, Error, OwnedBuf};
+    /// use musli_zerocopy::slice::Slice;
+    ///
+    /// fn generic<S>(buf: &Buf, slice: S) -> Result<(), Error>
+    /// where
+    ///     S: Slice<Item = i32>
+    /// {
+    ///     let two = slice.get(2).expect("Missing element 2");
+    ///     assert_eq!(buf.load(two)?, &3);
+    ///
+    ///     assert!(slice.get(4).is_none());
+    ///     Ok(())
+    /// }
+    ///
+    /// let mut buf = OwnedBuf::new();
+    /// let slice = buf.store_slice(&[1, 2, 3, 4]);
+    ///
+    /// generic(&buf, slice)?;
+    /// # Ok::<_, musli_zerocopy::Error>(())
+    /// ```
     fn get(self, index: usize) -> Option<Self::ItemRef>;
 
-    /// Split the slice at the given position.
+    /// Split the slice at the given position `at`.
     ///
     /// # Panics
     ///
     /// This panics if the given range is out of bounds.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use musli_zerocopy::{Buf, Error, OwnedBuf};
+    /// use musli_zerocopy::slice::Slice;
+    ///
+    /// fn generic<S>(buf: &Buf, slice: S) -> Result<(), Error>
+    /// where
+    ///     S: Slice<Item = i32>
+    /// {
+    ///     let (a, b) = slice.split_at(3);
+    ///     let (c, d) = slice.split_at(4);
+    ///
+    ///     assert_eq!(buf.load(a)?, &[1, 2, 3]);
+    ///     assert_eq!(buf.load(b)?, &[4]);
+    ///     assert_eq!(buf.load(c)?, &[1, 2, 3, 4]);
+    ///     assert_eq!(buf.load(d)?, &[]);
+    ///     Ok(())
+    /// }
+    ///
+    /// let mut buf = OwnedBuf::new();
+    /// let slice = buf.store_slice(&[1, 2, 3, 4]);
+    ///
+    /// buf.align_in_place();
+    ///
+    /// generic(&buf, slice)?;
+    /// # Ok::<_, musli_zerocopy::Error>(())
+    /// ```
     fn split_at(self, at: usize) -> (Self, Self);
 
-    /// Access an item in the slice in an unchecked manner.
+    /// Get an unchecked reference directly out of the slice without validation.
+    ///
+    /// This avoids having to validate every element in a slice in order to
+    /// address them.
+    ///
+    /// In contrast to [`get()`], this does not check that the index is within
+    /// the bounds of the current slice, all though it's not unsafe since it
+    /// cannot lead to anything inherently unsafe. Only garbled data.
+    ///
+    /// [`get()`]: Slice::get
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use musli_zerocopy::{Buf, Error, Ref, OwnedBuf};
+    /// use musli_zerocopy::slice::Slice;
+    ///
+    /// // A method generic over a specific slice implementation.
+    /// fn generic<S>(buf: &Buf, slice: S) -> Result<(), Error>
+    /// where
+    ///     S: Slice<Item = i32>
+    /// {
+    ///     let two = slice.get_unchecked(2);
+    ///     assert_eq!(buf.load(two)?, &3);
+    ///
+    ///     let oob = slice.get_unchecked(4);
+    ///     assert!(buf.load(oob).is_err());
+    ///     Ok(())
+    /// }
+    ///
+    /// let mut buf = OwnedBuf::new();
+    ///
+    /// let slice = buf.store_slice(&[1, 2, 3, 4]);
+    /// generic(&buf, slice)?;
+    /// # Ok::<_, musli_zerocopy::Error>(())
+    /// ```
     fn get_unchecked(self, index: usize) -> Self::ItemRef;
 
-    /// The length of a slice.
+    /// Return the number of elements in the slice.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use musli_zerocopy::Ref;
+    /// use musli_zerocopy::slice::Slice;
+    ///
+    /// // A method generic over a specific slice implementation.
+    /// fn generic<S>(slice: S) where S: Slice {
+    ///     assert_eq!(slice.len(), 2);
+    /// }
+    ///
+    /// let slice = Ref::<[i32]>::with_metadata(0, 2);
+    /// generic(slice);
+    /// ```
     fn len(self) -> usize;
 
     /// Test if the slice is empty.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use musli_zerocopy::Ref;
+    /// use musli_zerocopy::slice::Slice;
+    ///
+    /// // A method generic over a specific slice implementation.
+    /// fn generic<S>(a: S, b: S) where S: Slice {
+    ///     assert!(a.is_empty());
+    ///     assert!(!b.is_empty());
+    /// }
+    ///
+    /// let a = Ref::<[u32]>::with_metadata(0, 0);
+    /// let b = Ref::<[u32]>::with_metadata(0, 2);
+    /// generic(a, b);
+    /// ```
     fn is_empty(self) -> bool;
 }
 
-impl<T, A: ByteOrder, B: Size> Slice<[T]> for Ref<[T], A, B>
+impl<T, A: ByteOrder, B: Size> Slice for Ref<[T], A, B>
 where
     T: ZeroCopy,
 {
+    type Item = T;
     type ItemRef = Ref<T, A, B>;
 
     #[inline]
