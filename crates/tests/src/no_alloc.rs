29d0fe408e62612664b47598a8c7a4b19d70a935
John-John Tedro <udoprog@tedro.se>
2024-04-09 23:41:55 +0200

    Use benchmarker macro in rkyv

--- crates/tests/src/no_alloc.rs
+++ crates/tests/src/no_alloc.rs
@@ -1,4 +1,4 @@
 const CAPACITY: usize = 4096;
 
 /// A no-std bytes buffer.
-pub type Bytes = musli_utils::exports::fixed::FixedBytes<CAPACITY>;
+pub type Bytes = musli_utils::fixed::FixedBytes<CAPACITY>;
