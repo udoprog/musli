4c0d0197999e5d1cd5df923156756a895d376252
John-John Tedro <udoprog@tedro.se>
2024-12-25 16:46:22 +0100

    Fix error messages

--- crates/musli/tests/empty_enum.rs
+++ crates/musli/tests/empty_enum.rs
@@ -8,14 +8,26 @@ enum EmptyEnum {}
 #[test]
 fn decode_empty() {
     let e = musli::storage::decode::<_, EmptyEnum>(&[][..]).unwrap_err();
-    assert_eq!(e.to_string(), "Cannot decode uninhabitable types");
+    assert_eq!(
+        e.to_string(),
+        "Type EmptyEnum cannot be decoded since it's uninhabitable"
+    );
 
     let e = musli::wire::decode::<_, EmptyEnum>(&[][..]).unwrap_err();
-    assert_eq!(e.to_string(), "Cannot decode uninhabitable types");
+    assert_eq!(
+        e.to_string(),
+        "Type EmptyEnum cannot be decoded since it's uninhabitable"
+    );
 
     let e = musli::descriptive::decode::<_, EmptyEnum>(&[][..]).unwrap_err();
-    assert_eq!(e.to_string(), "Cannot decode uninhabitable types");
+    assert_eq!(
+        e.to_string(),
+        "Type EmptyEnum cannot be decoded since it's uninhabitable"
+    );
 
     let e = musli::json::from_slice::<EmptyEnum>(&[][..]).unwrap_err();
-    assert_eq!(e.to_string(), "Cannot decode uninhabitable types");
+    assert_eq!(
+        e.to_string(),
+        "Type EmptyEnum cannot be decoded since it's uninhabitable"
+    );
 }
