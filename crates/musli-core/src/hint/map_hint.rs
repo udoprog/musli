39386fe294b5bc8d2c94c967849a9fbb60557474
John-John Tedro <udoprog@tedro.se>
2025-02-23 20:03:08 +0100

    Seal hint types

--- crates/musli-core/src/hint/map_hint.rs
+++ crates/musli-core/src/hint/map_hint.rs
@@ -1,27 +1,33 @@
 use crate::de::SizeHint;
 use crate::Context;
 
+mod sealed {
+    pub trait Sealed {}
+    impl<T, M> Sealed for crate::__priv::EncodeMapHint<'_, T, M> where T: ?Sized {}
+    impl Sealed for usize {}
+    impl Sealed for Option<usize> {}
+}
+
 /// A size hint passed in when encoding or decoding a map.
-pub trait MapHint: Sized {
-    /// Get the map hint.
+pub trait MapHint: Sized + self::sealed::Sealed {
+    /// Get an optional map hint.
     fn get(self) -> Option<usize>;
 
-    /// Require the size of the map.
+    /// Require a map hint or raise an error indicating that the format doesn't
+    /// support maps of unknown size if one is not present.
     #[inline]
     fn require<C>(self, cx: C) -> Result<usize, C::Error>
     where
         C: Context,
     {
         let Some(size) = self.get() else {
-            return Err(
-                cx.message("Format cannot handle map types with an unknown number of entries")
-            );
+            return Err(cx.message("Format cannot handle maps with an unknown number of entries"));
         };
 
         Ok(size)
     }
 
-    /// The size hint for the map hint.
+    /// Coerce into a [`SizeHint`].
     #[inline]
     fn size_hint(self) -> SizeHint {
         match self.get() {
@@ -35,6 +41,19 @@ impl MapHint for usize {
     #[inline]
     fn get(self) -> Option<usize> {
         Some(self)
+    }
+
+    #[inline]
+    fn require<C>(self, _: C) -> Result<usize, C::Error>
+    where
+        C: Context,
+    {
+        Ok(self)
+    }
+
+    #[inline]
+    fn size_hint(self) -> SizeHint {
+        SizeHint::exact(self)
     }
 }
 
