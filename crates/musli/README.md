c089e9b2290e3e3d0441e5b61c8d6a7b3836515b
John-John Tedro <udoprog@tedro.se>
2025-08-30 04:11:20 +0200

    Release 0.0.145

--- crates/musli/README.md
+++ crates/musli/README.md
@@ -41,7 +41,7 @@ to use:
 
 ```toml
 [dependencies]
-musli = { version = "0.0.144", features = ["storage"] }
+musli = { version = "0.0.145", features = ["storage"] }
 ```
 
 <br>
--- crates/musli/src/lib.rs
+++ crates/musli/src/lib.rs
@@ -38,7 +38,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! musli = { version = "0.0.144", features = ["storage"] }
+//! musli = { version = "0.0.145", features = ["storage"] }
 //! ```
 //!
 //! <br>
