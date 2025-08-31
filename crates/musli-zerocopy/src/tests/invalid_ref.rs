93c5be1bd9653d2ab9d22da6ea7ef535c54af668
bluurryy <164359728+bluurryy@users.noreply.github.com>
2025-08-31 10:21:06 +0200

    add test for byte order

--- crates/musli-zerocopy/src/tests/invalid_ref.rs
+++ crates/musli-zerocopy/src/tests/invalid_ref.rs
@@ -1,4 +1,9 @@
-use crate::{Ref, endian::Native};
+use musli_zerocopy_macros::ZeroCopy;
+
+use crate::{
+    Ref,
+    endian::{Big, Little, Native},
+};
 
 const MAX: usize = isize::MAX as usize;
 
@@ -11,4 +16,23 @@ fn test_slice_with_metadata() {
 
     Ref::<[u16], Native, usize>::try_with_metadata(0, MAX / 2).unwrap();
     Ref::<[u16], Native, usize>::try_with_metadata(0, (MAX / 2) + 1).unwrap_err();
+}
+
+#[test]
+fn test_metadata_byte_order() {
+    #[derive(ZeroCopy)]
+    #[zero_copy(crate)]
+    #[repr(C)]
+    struct BigStruct([u8; 4096]);
+
+    const BIG_MAX_LEN: usize = MAX / size_of::<BigStruct>();
+
+    Ref::<[BigStruct], Native, usize>::try_with_metadata(0, BIG_MAX_LEN).unwrap();
+    Ref::<[BigStruct], Native, usize>::try_with_metadata(0, BIG_MAX_LEN + 1).unwrap_err();
+
+    Ref::<[BigStruct], Little, usize>::try_with_metadata(0, BIG_MAX_LEN).unwrap();
+    Ref::<[BigStruct], Little, usize>::try_with_metadata(0, BIG_MAX_LEN + 1).unwrap_err();
+
+    Ref::<[BigStruct], Big, usize>::try_with_metadata(0, BIG_MAX_LEN).unwrap();
+    Ref::<[BigStruct], Big, usize>::try_with_metadata(0, BIG_MAX_LEN + 1).unwrap_err();
 }
