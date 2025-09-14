722f7b32bc230d869de44e45bb242af87759bfd0
John-John Tedro <udoprog@tedro.se>
2025-09-14 18:33:08 +0200

    Release musli-0.0.149

--- crates/musli/src/lib.rs
+++ crates/musli/src/lib.rs
@@ -38,7 +38,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! musli = { version = "0.0.148", features = ["storage"] }
+//! musli = { version = "0.0.149", features = ["storage"] }
 //! ```
 //!
 //! <br>
