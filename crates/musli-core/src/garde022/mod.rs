2ddafff8b16ed2468d8841696c472f6994456757
sirati <git.public@sirati.me>
2025-10-27 15:02:22 +0100

    remove clone from copy Decoder::Cx and cargo fmt

--- crates/musli-core/src/garde022/mod.rs
+++ crates/musli-core/src/garde022/mod.rs
@@ -1,21 +1,20 @@
-
 #![cfg(feature = "garde022")]
 #![cfg_attr(doc_cfg, doc(cfg(feature = "garde022")))]
 
-use std::ops::Deref;
+use crate::{Allocator, Context, Decode, Decoder, Encode, Encoder};
 use garde022::{Unvalidated, Valid, Validate};
-use crate::{Decode, Decoder, Encode, Encoder, Allocator, Context};
+use std::ops::Deref;
 
 impl<TValid, M> Encode<M> for Valid<TValid>
 where
-    TValid: Validate + Encode<M>
+    TValid: Validate + Encode<M>,
 {
     const IS_BITWISE_ENCODE: bool = TValid::IS_BITWISE_ENCODE;
     type Encode = TValid::Encode;
 
     fn encode<E>(&self, encoder: E) -> Result<(), E::Error>
     where
-        E: Encoder<Mode=M>
+        E: Encoder<Mode = M>,
     {
         TValid::encode(self.deref(), encoder)
     }
@@ -32,38 +31,36 @@ where
 impl<'de, TValid, M, A> Decode<'de, M, A> for Unvalidated<TValid>
 where
     TValid: Validate + Decode<'de, M, A>,
-    A: Allocator
+    A: Allocator,
 {
     const IS_BITWISE_DECODE: bool = TValid::IS_BITWISE_DECODE;
 
     fn decode<D>(decoder: D) -> Result<Self, D::Error>
     where
-        D: Decoder<'de, Mode=M, Allocator=A>
+        D: Decoder<'de, Mode = M, Allocator = A>,
     {
         TValid::decode(decoder).map(Unvalidated::new)
     }
 }
 
-
 impl<'de, TValid, M, A> Decode<'de, M, A> for Valid<TValid>
 where
     TValid: Validate<Context: Default> + Decode<'de, M, A>,
-    A: Allocator
+    A: Allocator,
 {
     const IS_BITWISE_DECODE: bool = TValid::IS_BITWISE_DECODE;
 
     fn decode<D>(decoder: D) -> Result<Self, D::Error>
     where
-        D: Decoder<'de, Mode=M, Allocator=A>
+        D: Decoder<'de, Mode = M, Allocator = A>,
     {
-        let cx = decoder.cx().clone();
+        let cx = decoder.cx();
         match Unvalidated::<TValid>::decode(decoder) {
             Ok(unvalidated) => match unvalidated.validate() {
                 Ok(valid) => Ok(valid),
                 Err(report) => Err(cx.custom(report)),
-            }
+            },
             Err(e) => Err(e),
         }
     }
 }
-
